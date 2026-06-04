eba7936862676a509a081f70ba1b05bb72dac0cd
Ashish khadse <106680563+Ashish0000777@users.noreply.github.com>
2026-06-03 19:50:27 -0700

    Update constants.js
    updated resume link

--- src/data/constants.js
+++ src/data/constants.js
@@ -14,7 +14,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Ashish0000777",
   resume:
-    "https://drive.google.com/file/d/1vpstjDVg_Z26PxqqQjD4RUgrmPsntYW7/view?usp=sharing",
+    "https://drive.google.com/file/d/17F1eeVpopoylkFVlO_wWJSaY2qrhwhVz/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ashish-khadase-912ba1238/",
   twitter: "https://x.com/Ashishkhadse_7",
   insta: "https://www.instagram.com/ashish_khadse7/",
